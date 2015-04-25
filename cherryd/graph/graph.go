b8aa5305f6e8d395b3381aefa866dee045e6fbef
Kitae Kim <superkkt@sds.co.kr>
2015-04-26 03:03:26 +0900

    Implement Kruskal's algorithm (see #5086)

---
+++ cherryd/graph/graph.go
@@ -0,0 +1,441 @@
+/*
+ * Cherry - An OpenFlow Controller
+ *
+ * Copyright (C) 2015 Samjung Data Service Co., Ltd.,
+ * Kitae Kim <superkkt@sds.co.kr>
+ */
+
+package graph
+
+import (
+	"container/list"
+	"errors"
+	"sort"
+	"sync"
+)
+
+type Vertex interface {
+	ID() string
+}
+
+type Edge interface {
+	ID() string
+	Vertexies() [2]Vertex
+	Weight() float64
+}
+
+type edge struct {
+	value   Edge
+	enabled bool
+}
+
+type vertex struct {
+	value Vertex
+	edges map[string]*edge
+}
+
+type Graph struct {
+	mutex     sync.Mutex
+	vertexies map[string]vertex
+	edges     map[string]*edge
+}
+
+func New() *Graph {
+	return &Graph{
+		vertexies: make(map[string]vertex),
+		edges:     make(map[string]*edge),
+	}
+}
+
+func (r *Graph) AddVertex(v Vertex) {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+
+	if v == nil {
+		panic("Graph: adding nil vertex")
+	}
+	// Check duplication
+	_, ok := r.vertexies[v.ID()]
+	if ok {
+		return
+	}
+
+	r.vertexies[v.ID()] = vertex{
+		value: v,
+		edges: make(map[string]*edge),
+	}
+	r.calculateMST()
+}
+
+func (r *Graph) removeEdge(e Edge) {
+	v := e.Vertexies()
+	v1, ok := r.vertexies[v[0].ID()]
+	if ok {
+		delete(v1.edges, e.ID())
+	}
+	v2, ok := r.vertexies[v[1].ID()]
+	if ok {
+		delete(v2.edges, e.ID())
+	}
+	delete(r.edges, e.ID())
+}
+
+func (r *Graph) RemoveVertex(v Vertex) {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+
+	if v == nil {
+		panic("Graph: removing nil vertex")
+	}
+
+	vertex, ok := r.vertexies[v.ID()]
+	if !ok {
+		return
+	}
+	for _, e := range vertex.edges {
+		r.removeEdge(e.value)
+	}
+	delete(r.vertexies, v.ID())
+	r.calculateMST()
+}
+
+func (r *Graph) AddEdge(e Edge) error {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+
+	if e == nil {
+		panic("Graph: adding nil edge")
+	}
+	// Check duplication
+	_, ok := r.edges[e.ID()]
+	if ok {
+		return nil
+	}
+
+	vertexies := e.Vertexies()
+	if vertexies[0] == nil || vertexies[1] == nil {
+		panic("Graph: adding an edge pointing to nil vertex")
+	}
+	first, ok1 := r.vertexies[vertexies[0].ID()]
+	second, ok2 := r.vertexies[vertexies[1].ID()]
+	if !ok1 || !ok2 {
+		return errors.New("Graph: adding an edge to unknown vertex")
+	}
+
+	edge := &edge{value: e}
+	r.edges[e.ID()] = edge
+	first.edges[e.ID()] = edge
+	second.edges[e.ID()] = edge
+	r.calculateMST()
+
+	return nil
+}
+
+func (r *Graph) RemoveEdge(e Edge) error {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+
+	if e == nil {
+		panic("Graph: removing nil edge")
+	}
+	_, ok := r.edges[e.ID()]
+	if !ok {
+		return nil
+	}
+
+	vertexies := e.Vertexies()
+	if vertexies[0] == nil || vertexies[1] == nil {
+		panic("Graph: removing an edge pointing to nil vertex")
+	}
+	first, ok1 := r.vertexies[vertexies[0].ID()]
+	second, ok2 := r.vertexies[vertexies[1].ID()]
+	if !ok1 || !ok2 {
+		return errors.New("Graph: removing an edge to unknown vertex")
+	}
+
+	delete(first.edges, e.ID())
+	delete(second.edges, e.ID())
+	delete(r.edges, e.ID())
+	r.calculateMST()
+
+	return nil
+}
+
+type sortedEdge []*edge
+
+func (r sortedEdge) Len() int {
+	return len(r)
+}
+
+func (r sortedEdge) Less(i, j int) bool {
+	if r[i].value.Weight() < r[j].value.Weight() {
+		return true
+	}
+
+	return false
+}
+
+func (r sortedEdge) Swap(i, j int) {
+	r[i], r[j] = r[j], r[i]
+}
+
+func (r *Graph) pickRootVertex() Vertex {
+	// Pick arbitrary vertex node that has at least one edge.
+	for _, v := range r.vertexies {
+		if len(v.edges) == 0 {
+			continue
+		}
+		return v.value
+	}
+
+	return nil
+}
+
+func (r *Graph) resetEdges() *list.List {
+	edges := make(sortedEdge, 0)
+	for _, v := range r.edges {
+		// Disable all edges
+		v.enabled = false
+		edges = append(edges, v)
+	}
+	sort.Sort(edges)
+
+	result := list.New()
+	for _, v := range edges {
+		result.PushBack(v)
+	}
+
+	return result
+}
+
+func (r *Graph) pickValidVertexies() []Vertex {
+	result := make([]Vertex, 0)
+	for _, v := range r.vertexies {
+		if len(v.edges) == 0 {
+			continue
+		}
+		result = append(result, v.value)
+	}
+
+	return result
+}
+
+func (r *Graph) makeClusters() map[string]*list.List {
+	result := make(map[string]*list.List)
+	for _, v := range r.vertexies {
+		l := list.New()
+		l.PushBack(v)
+		result[v.value.ID()] = l
+	}
+
+	return result
+}
+
+func mergeCluster(clusters map[string]*list.List, l1, l2 *list.List) {
+	v := list.New()
+	v.PushBackList(l1)
+	v.PushBackList(l2)
+
+	for elem := v.Front(); elem != nil; elem = elem.Next() {
+		vertex := elem.Value.(vertex)
+		clusters[vertex.value.ID()] = v
+	}
+}
+
+// calculateMST finds a minimum spanning tree of this graph using Kruskal's algorithm.
+// A caller should lock the mutex before calling this function.
+func (r *Graph) calculateMST() {
+	if len(r.edges) == 0 || len(r.vertexies) == 0 {
+		return
+	}
+
+	// FIXME: Use priority queue instead of sorting!
+	edges := r.resetEdges()
+	clusters := r.makeClusters()
+
+	count := 0
+	for count < len(r.vertexies)-1 {
+		if edges.Len() == 0 {
+			break
+		}
+
+		// Pop the minimum weighted edge
+		elem := edges.Front()
+		e := elem.Value.(*edge)
+		edges.Remove(elem)
+
+		vertexies := e.value.Vertexies()
+		v1, ok := clusters[vertexies[0].ID()]
+		if !ok {
+			panic("Graph: invalid edge pointing an unknown vertex")
+		}
+		v2, ok := clusters[vertexies[1].ID()]
+		if !ok {
+			panic("Graph: invalid edge pointing an unknown vertex")
+		}
+
+		// Prevent a loop
+		if v1 == v2 {
+			continue
+		}
+		// Found new edge to be included in MST
+		mergeCluster(clusters, v1, v2)
+		e.enabled = true
+		count++
+	}
+
+	/*
+		if len(r.edges) == 0 || len(r.vertexies) == 0 {
+			return
+		}
+
+		edges := r.makeSortedEdges()
+		Vp := make(map[string]Vertex)
+
+		V := r.pickValidVertexies()
+		root := r.pickRootVertex()
+		if len(V) == 0 || root == nil {
+			// There is no spanning tree for this graph.
+			return
+		}
+
+		prevLen := 0
+		// Initial vertex node
+		Vp[root.ID()] = root
+		// Implementation of Prim's algorithm
+		for len(V) != len(Vp) && prevLen != len(Vp) {
+			prevLen = len(Vp)
+			var next *list.Element
+			for elem := edges.Front(); elem != nil; elem = next {
+				next = elem.Next()
+				e := elem.Value.(*edge)
+
+				vertexies := e.value.Vertexies()
+				_, first := Vp[vertexies[0].ID()]
+				_, second := Vp[vertexies[1].ID()]
+				// New candidate vertex?
+				if (first && second) || (!first && !second) {
+					continue
+				}
+				if first {
+					Vp[vertexies[1].ID()] = vertexies[1]
+				} else {
+					Vp[vertexies[0].ID()] = vertexies[0]
+				}
+
+				// Found new vertex
+				e.enabled = true
+				edges.Remove(elem)
+				break
+			}
+		}
+
+		// Disable all remaining edges
+		for elem := edges.Front(); elem != nil; elem = elem.Next() {
+			e := elem.Value.(*edge)
+			e.enabled = false
+		}
+	*/
+}
+
+type queue struct {
+	list *list.List
+}
+
+func newQueue() *queue {
+	return &queue{list.New()}
+}
+
+func (r *queue) enqueue(v interface{}) {
+	r.list.PushBack(v)
+}
+
+func (r *queue) dequeue() interface{} {
+	v := r.list.Front()
+	if v != nil {
+		r.list.Remove(v)
+	}
+	return v.Value
+}
+
+func (r *queue) length() int {
+	return r.list.Len()
+}
+
+type Path struct {
+	V Vertex
+	E Edge
+}
+
+func (r *Graph) FindPath(src, dst Vertex) []Path {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+
+	if len(r.vertexies) == 0 || len(r.edges) == 0 {
+		return []Path{}
+	}
+
+	result := make([]Path, 0)
+	visited := make(map[string]bool)
+	prev := make(map[string]Path)
+
+	queue := newQueue()
+	queue.enqueue(src)
+	visited[src.ID()] = true
+
+	// Implementation of BFS algorithm
+	for queue.length() > 0 {
+		v := queue.dequeue()
+		if v == nil {
+			panic("Graph: nil element is fetched from the queue")
+		}
+
+		vertex, ok := r.vertexies[v.(Vertex).ID()]
+		if !ok {
+			return []Path{}
+		}
+		for _, w := range vertex.edges {
+			// We only use edges that belong to MST.
+			if w.enabled == false {
+				continue
+			}
+			vertexies := w.value.Vertexies()
+			next := vertexies[0]
+			if vertexies[0].ID() == vertex.value.ID() {
+				next = vertexies[1]
+			}
+			if _, ok := visited[next.ID()]; ok {
+				continue
+			}
+			visited[next.ID()] = true
+			prev[next.ID()] = Path{V: vertex.value, E: w.value}
+			queue.enqueue(next)
+		}
+	}
+
+	u := dst
+	for {
+		path, ok := prev[u.ID()]
+		if !ok {
+			break
+		}
+		result = append(result, path)
+		u = path.V
+	}
+
+	return reverse(result)
+}
+
+func reverse(data []Path) []Path {
+	length := len(data)
+	if length == 0 {
+		return data
+	}
+
+	result := make([]Path, length)
+	for i, j := 0, length-1; i < length; i, j = i+1, j-1 {
+		result[i] = data[j]
+	}
+
+	return result
+}
---
+++ cherryd/graph/graph_test.go
@@ -0,0 +1,534 @@
+/*
+ * Cherry - An OpenFlow Controller
+ *
+ * Copyright (C) 2015 Samjung Data Service Co., Ltd.,
+ * Kitae Kim <superkkt@sds.co.kr>
+ */
+
+package graph
+
+import (
+	"fmt"
+	"testing"
+)
+
+type node struct {
+	dpid string
+}
+
+func (r node) ID() string {
+	return fmt.Sprintf("%v", r.dpid)
+}
+
+type point struct {
+	dpid string
+	port uint32
+}
+
+type link struct {
+	points [2]point
+	weight float64
+}
+
+func (r link) ID() string {
+	return fmt.Sprintf("%v:%v/%v:%p", r.points[0].dpid, r.points[0].port, r.points[1].dpid, r.points[1].port)
+}
+
+func (r link) Vertexies() [2]Vertex {
+	v1 := node{r.points[0].dpid}
+	v2 := node{r.points[1].dpid}
+	var result [2]Vertex
+	result[0] = v1
+	result[1] = v2
+	return result
+}
+
+func (r link) Weight() float64 {
+	return r.weight
+}
+
+func printEnabledEdges(g *Graph) (int, float64) {
+	count := 0
+	weight := 0.0
+	for _, v := range g.edges {
+		if v.enabled == false {
+			continue
+		}
+		fmt.Printf("Edge: %+v\n", v.value)
+		count++
+		weight += v.value.Weight()
+	}
+
+	return count, weight
+}
+
+func TestInvalidMST(t *testing.T) {
+	graph := New()
+	graph.AddVertex(node{"a"})
+	graph.calculateMST()
+	c, _ := printEnabledEdges(graph)
+	if c != 0 {
+		t.Fatalf("Unexpected MST: expected len=0, got=%v", c)
+	}
+
+	e := link{
+		points: [2]point{point{"a", 1}, point{"b", 1}},
+		weight: 2,
+	}
+	if err := graph.AddEdge(e); err == nil {
+		t.Fatal("Expected error, but not occurred!")
+	}
+}
+
+func TestRemoveVertex(t *testing.T) {
+	graph := New()
+	graph.AddVertex(node{"a"})
+	graph.AddVertex(node{"b"})
+	e := link{
+		points: [2]point{point{"a", 1}, point{"b", 1}},
+		weight: 2,
+	}
+	if err := graph.AddEdge(e); err != nil {
+		t.Fatal(err)
+	}
+	graph.RemoveVertex(node{"a"})
+	if len(graph.vertexies) != 1 {
+		t.Fatalf("Expected node length is 1, got=%v\n", len(graph.vertexies))
+	}
+	if len(graph.edges) != 0 {
+		t.Fatalf("Expected edge length is 0, got=%v\n", len(graph.edges))
+	}
+	v := graph.vertexies["b"]
+	if len(v.edges) != 0 {
+		t.Fatalf("Expected edge length is 0, got=%v\n", len(v.edges))
+	}
+}
+
+func TestRemoveEdges(t *testing.T) {
+	graph := New()
+	graph.AddVertex(node{"a"})
+	graph.AddVertex(node{"b"})
+	e := link{
+		points: [2]point{point{"a", 1}, point{"b", 1}},
+		weight: 2,
+	}
+	if err := graph.AddEdge(e); err != nil {
+		t.Fatal(err)
+	}
+	graph.RemoveEdge(e)
+	if len(graph.edges) != 0 {
+		t.Fatalf("Expected edge length is 0, got=%v\n", len(graph.edges))
+	}
+	a := graph.vertexies["a"]
+	b := graph.vertexies["b"]
+	if len(a.edges) != 0 || len(b.edges) != 0 {
+		t.Fatalf("Expected # of edges is 0/0, got=%v/%v\n", len(a.edges), len(b.edges))
+	}
+
+	if err := graph.AddEdge(e); err != nil {
+		t.Fatal(err)
+	}
+	if err := graph.AddEdge(e); err != nil {
+		t.Fatal(err)
+	}
+	if len(a.edges) != 1 || len(b.edges) != 1 {
+		t.Fatalf("Expected # of edges is 1/1, got=%v/%v\n", len(a.edges), len(b.edges))
+	}
+	graph.RemoveEdge(e)
+	if len(graph.edges) != 0 {
+		t.Fatalf("Expected edge length is 0, got=%v\n", len(graph.edges))
+	}
+	if len(a.edges) != 0 || len(b.edges) != 0 {
+		t.Fatalf("Expected # of edges is 0/0, got=%v/%v\n", len(a.edges), len(b.edges))
+	}
+}
+
+func TestDuplicatedEdges(t *testing.T) {
+	graph := New()
+	graph.AddVertex(node{"a"})
+	graph.AddVertex(node{"b"})
+	e := link{
+		points: [2]point{point{"a", 1}, point{"b", 1}},
+		weight: 2,
+	}
+	if err := graph.AddEdge(e); err != nil {
+		t.Fatal(err)
+	}
+	if err := graph.AddEdge(e); err != nil {
+		t.Fatal(err)
+	}
+
+	graph.calculateMST()
+	c, w := printEnabledEdges(graph)
+	if c != 1 || w != 2 {
+		t.Fatalf("Unexpected MST: expected=1/2, got=%v/%v", c, w)
+	}
+}
+
+func TestMST0(t *testing.T) {
+	graph := New()
+	graph.AddVertex(node{"a"})
+	graph.AddVertex(node{"b"})
+	graph.AddVertex(node{"c"})
+	graph.AddVertex(node{"d"})
+
+	edges := make([]link, 0)
+	edges = append(edges, link{
+		points: [2]point{point{"a", 1}, point{"b", 1}},
+		weight: 2,
+	})
+	edges = append(edges, link{
+		points: [2]point{point{"c", 1}, point{"d", 3}},
+		weight: 3,
+	})
+
+	for _, v := range edges {
+		if err := graph.AddEdge(v); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	for i := 0; i < 100; i++ {
+		graph.calculateMST()
+		c, w := printEnabledEdges(graph)
+		if c != 2 || w != 5 {
+			t.Fatalf("Unexpected MST: expected=2/5, got=%v/%v", c, w)
+		}
+
+		path := graph.FindPath(node{"a"}, node{"b"})
+		fmt.Printf("Path: %+v\n", path)
+		total := 0.0
+		for _, v := range path {
+			total += v.E.Weight()
+		}
+		if len(path) != 1 || total != 2 {
+			t.Fatalf("Unexpected Path: expected=1/2, got=%v/%v", len(path), total)
+		}
+
+		path = graph.FindPath(node{"d"}, node{"c"})
+		fmt.Printf("Path: %+v\n", path)
+		total = 0.0
+		for _, v := range path {
+			total += v.E.Weight()
+		}
+		if len(path) != 1 || total != 3 {
+			t.Fatalf("Unexpected Path: expected=1/3, got=%v/%v", len(path), total)
+		}
+
+		path = graph.FindPath(node{"b"}, node{"c"})
+		fmt.Printf("Path: %+v\n", path)
+		total = 0.0
+		for _, v := range path {
+			total += v.E.Weight()
+		}
+		if len(path) != 0 || total != 0 {
+			t.Fatalf("Unexpected Path: expected=0/0, got=%v/%v", len(path), total)
+		}
+	}
+}
+
+func TestMST1(t *testing.T) {
+	graph := New()
+	graph.AddVertex(node{"a"})
+	graph.AddVertex(node{"b"})
+	graph.AddVertex(node{"c"})
+	graph.AddVertex(node{"d"})
+
+	edges := make([]link, 0)
+	edges = append(edges, link{
+		points: [2]point{point{"a", 1}, point{"b", 1}},
+		weight: 2,
+	})
+	edges = append(edges, link{
+		points: [2]point{point{"a", 2}, point{"d", 1}},
+		weight: 1,
+	})
+	edges = append(edges, link{
+		points: [2]point{point{"b", 2}, point{"d", 2}},
+		weight: 3,
+	})
+	edges = append(edges, link{
+		points: [2]point{point{"c", 1}, point{"d", 3}},
+		weight: 3,
+	})
+
+	for _, v := range edges {
+		if err := graph.AddEdge(v); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	for i := 0; i < 100; i++ {
+		graph.calculateMST()
+		c, w := printEnabledEdges(graph)
+		if c != 3 || w != 6 {
+			t.Fatalf("Unexpected MST: expected=3/6, got=%v/%v", c, w)
+		}
+
+		path := graph.FindPath(node{"b"}, node{"c"})
+		fmt.Printf("Path: %+v\n", path)
+		total := 0.0
+		for _, v := range path {
+			total += v.E.Weight()
+		}
+		if len(path) != 3 || total != 6 {
+			t.Fatalf("Unexpected Path: expected=3/6, got=%v/%v", len(path), total)
+		}
+	}
+}
+
+func TestMST2(t *testing.T) {
+	graph := New()
+	graph.AddVertex(node{"a"})
+	graph.AddVertex(node{"b"})
+	graph.AddVertex(node{"c"})
+	graph.AddVertex(node{"d"})
+	graph.AddVertex(node{"e"})
+	graph.AddVertex(node{"f"})
+	graph.AddVertex(node{"g"})
+
+	edges := make([]link, 0)
+	edges = append(edges, link{
+		points: [2]point{point{"a", 1}, point{"b", 1}},
+		weight: 4,
+	})
+	edges = append(edges, link{
+		points: [2]point{point{"a", 2}, point{"c", 1}},
+		weight: 8,
+	})
+	edges = append(edges, link{
+		points: [2]point{point{"b", 2}, point{"c", 2}},
+		weight: 9,
+	})
+	edges = append(edges, link{
+		points: [2]point{point{"b", 1}, point{"d", 3}},
+		weight: 8,
+	})
+	edges = append(edges, link{
+		points: [2]point{point{"b", 1}, point{"e", 3}},
+		weight: 10,
+	})
+	edges = append(edges, link{
+		points: [2]point{point{"c", 1}, point{"d", 3}},
+		weight: 2,
+	})
+	edges = append(edges, link{
+		points: [2]point{point{"c", 1}, point{"f", 3}},
+		weight: 1,
+	})
+	edges = append(edges, link{
+		points: [2]point{point{"d", 1}, point{"e", 3}},
+		weight: 7,
+	})
+	edges = append(edges, link{
+		points: [2]point{point{"d", 1}, point{"f", 3}},
+		weight: 9,
+	})
+	edges = append(edges, link{
+		points: [2]point{point{"e", 1}, point{"f", 3}},
+		weight: 5,
+	})
+	edges = append(edges, link{
+		points: [2]point{point{"e", 1}, point{"g", 3}},
+		weight: 6,
+	})
+	edges = append(edges, link{
+		points: [2]point{point{"f", 1}, point{"g", 3}},
+		weight: 2,
+	})
+
+	for _, v := range edges {
+		if err := graph.AddEdge(v); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	for i := 0; i < 100; i++ {
+		graph.calculateMST()
+		c, w := printEnabledEdges(graph)
+		if c != 6 || w != 22 {
+			t.Fatalf("Unexpected MST: expected=6/22, got=%v/%v", c, w)
+		}
+
+		path := graph.FindPath(node{"d"}, node{"e"})
+		fmt.Printf("Path: %+v\n", path)
+		total := 0.0
+		for _, v := range path {
+			total += v.E.Weight()
+		}
+		if len(path) != 3 || total != 8 {
+			t.Fatalf("Unexpected Path: expected=3/8, got=%v/%v", len(path), total)
+		}
+	}
+}
+
+func TestMST3(t *testing.T) {
+	graph := New()
+	graph.AddVertex(node{"0"})
+	graph.AddVertex(node{"1"})
+	graph.AddVertex(node{"2"})
+	graph.AddVertex(node{"3"})
+	graph.AddVertex(node{"4"})
+	graph.AddVertex(node{"5"})
+	graph.AddVertex(node{"6"})
+	graph.AddVertex(node{"7"})
+	graph.AddVertex(node{"8"})
+
+	edges := make([]link, 0)
+	edges = append(edges, link{
+		points: [2]point{point{"0", 1}, point{"1", 1}},
+		weight: 4,
+	})
+	edges = append(edges, link{
+		points: [2]point{point{"0", 2}, point{"7", 1}},
+		weight: 8,
+	})
+	edges = append(edges, link{
+		points: [2]point{point{"1", 2}, point{"2", 2}},
+		weight: 9,
+	})
+	edges = append(edges, link{
+		points: [2]point{point{"1", 1}, point{"7", 3}},
+		weight: 11,
+	})
+	edges = append(edges, link{
+		points: [2]point{point{"2", 1}, point{"3", 3}},
+		weight: 7,
+	})
+	edges = append(edges, link{
+		points: [2]point{point{"2", 1}, point{"5", 3}},
+		weight: 4,
+	})
+	edges = append(edges, link{
+		points: [2]point{point{"2", 1}, point{"8", 3}},
+		weight: 2,
+	})
+	edges = append(edges, link{
+		points: [2]point{point{"3", 1}, point{"4", 3}},
+		weight: 9,
+	})
+	edges = append(edges, link{
+		points: [2]point{point{"3", 1}, point{"5", 3}},
+		weight: 14,
+	})
+	edges = append(edges, link{
+		points: [2]point{point{"4", 1}, point{"5", 3}},
+		weight: 10,
+	})
+	edges = append(edges, link{
+		points: [2]point{point{"5", 1}, point{"6", 3}},
+		weight: 2,
+	})
+	edges = append(edges, link{
+		points: [2]point{point{"6", 1}, point{"7", 3}},
+		weight: 1,
+	})
+	edges = append(edges, link{
+		points: [2]point{point{"6", 1}, point{"8", 3}},
+		weight: 6,
+	})
+	edges = append(edges, link{
+		points: [2]point{point{"7", 1}, point{"8", 3}},
+		weight: 7,
+	})
+
+	for _, v := range edges {
+		if err := graph.AddEdge(v); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	for i := 0; i < 100; i++ {
+		graph.calculateMST()
+		c, w := printEnabledEdges(graph)
+		if c != 8 || w != 37 {
+			t.Fatalf("Unexpected MST: expected=8/37, got=%v/%v", c, w)
+		}
+
+		path := graph.FindPath(node{"0"}, node{"8"})
+		fmt.Printf("Path: %+v\n", path)
+		total := 0.0
+		for _, v := range path {
+			total += v.E.Weight()
+		}
+		if len(path) != 5 || total != 17 {
+			t.Fatalf("Unexpected Path: expected=5/17, got=%v/%v", len(path), total)
+		}
+	}
+}
+
+func TestMST4(t *testing.T) {
+	graph := New()
+	graph.AddVertex(node{"0"})
+	graph.AddVertex(node{"1"})
+	graph.AddVertex(node{"2"})
+	graph.AddVertex(node{"3"})
+	graph.AddVertex(node{"4"})
+	graph.AddVertex(node{"5"})
+
+	edges := make([]link, 0)
+	edges = append(edges, link{
+		points: [2]point{point{"0", 1}, point{"1", 1}},
+		weight: 3,
+	})
+	edges = append(edges, link{
+		points: [2]point{point{"0", 2}, point{"2", 1}},
+		weight: 1,
+	})
+	edges = append(edges, link{
+		points: [2]point{point{"0", 2}, point{"3", 2}},
+		weight: 6,
+	})
+	edges = append(edges, link{
+		points: [2]point{point{"1", 1}, point{"2", 3}},
+		weight: 5,
+	})
+	edges = append(edges, link{
+		points: [2]point{point{"1", 1}, point{"4", 3}},
+		weight: 3,
+	})
+	edges = append(edges, link{
+		points: [2]point{point{"2", 1}, point{"3", 3}},
+		weight: 5,
+	})
+	edges = append(edges, link{
+		points: [2]point{point{"2", 1}, point{"4", 3}},
+		weight: 6,
+	})
+	edges = append(edges, link{
+		points: [2]point{point{"2", 1}, point{"5", 3}},
+		weight: 4,
+	})
+	edges = append(edges, link{
+		points: [2]point{point{"3", 1}, point{"5", 3}},
+		weight: 2,
+	})
+	edges = append(edges, link{
+		points: [2]point{point{"4", 1}, point{"5", 3}},
+		weight: 6,
+	})
+
+	for _, v := range edges {
+		if err := graph.AddEdge(v); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	for i := 0; i < 100; i++ {
+		graph.calculateMST()
+		c, w := printEnabledEdges(graph)
+		if c != 5 || w != 13 {
+			t.Fatalf("Unexpected MST: expected=5/13, got=%v/%v", c, w)
+		}
+
+		path := graph.FindPath(node{"1"}, node{"3"})
+		fmt.Printf("Path: %+v\n", path)
+		total := 0.0
+		for _, v := range path {
+			total += v.E.Weight()
+		}
+		if len(path) != 4 || total != 10 {
+			t.Fatalf("Unexpected Path: expected=4/10, got=%v/%v", len(path), total)
+		}
+	}
+}
