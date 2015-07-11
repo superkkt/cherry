8b733aa3b4cb9798726d133f0909feae18d56a1d
Kitae Kim <superkkt@sds.co.kr>
2015-07-12 00:56:53 +0900

    Remove SIGHUP from the Docker entrypoint script

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -12,7 +12,7 @@ shutdown()
 
 # Use the trap if you need to also do manual cleanup after the service is stopped,
 # or need to start multiple services in the one container
-trap 'shutdown; exit' SIGHUP SIGINT SIGQUIT SIGTERM
+trap 'shutdown; exit' SIGINT SIGQUIT SIGTERM
 
 # Start services in background here
 /etc/init.d/rsyslog start
